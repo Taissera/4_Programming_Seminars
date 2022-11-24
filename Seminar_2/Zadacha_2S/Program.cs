0d41f72157ab4702bd53f8501afb696e7bfb6c9e
Taissera <taissera@ya.ru>
2022-11-25 01:44:22 +0300

    Zadacha_2S prepod

--- Seminar_2/Zadacha_2S/Program.cs
+++ Seminar_2/Zadacha_2S/Program.cs
@@ -4,3 +4,18 @@
 12 -> 2
 85 -> 8
 */
+
+Random rnd = new Random();
+int number = rnd.Next(10, 100); // [10, 100)
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+Console.WriteLine($"Рандомное число: {number}");
+
+if (firstDigit > secondDigit)
+    Console.WriteLine($"Цифра {firstDigit} больше чем {secondDigit}");
+else
+    if (secondDigit > firstDigit)
+    Console.WriteLine($"Цифра {secondDigit} больше чем {firstDigit}");
+else
+    Console.WriteLine($"Цифры равны");
