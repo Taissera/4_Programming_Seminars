e5f0c1e3ef7127d5b6acebe82e82a3829a6e4a41
taissera <taissera@ya.ru>
2022-12-09 22:54:29 +0300

    Seminar 6

---
+++ Seminar_6/Zadacha_39/Program.cs
@@ -0,0 +1,74 @@
+﻿/*
+Задача №39: Напишите программу, которая перевернёт одномерный массив
+(последний элемент будет на первом месте, а первый - на последней и т.д.)
+[1 2 3 4 5] -> [5 4 3 2 1]
+[6 7 3 6] -> [6 3 7 6]
+*/
+
+/*
+    1. метод, который будет вызыввать заполненный массив случайными числами
+    2. метод, который будет печатать массив
+    3. получение числа с консоли, для того, чтобы задать размерность массива
+    4. метод, который будет переворачивать массив
+*/
+
+// метод, который будет выдавать заполненный массив случайными числами
+int[] InitArray (int dimension)
+{
+    int[] array = new int [dimension];
+    Random rnd = new Random();
+    for (int i = 0; i < dimension; i++)
+    {
+        array[i] = rnd.Next(1,9);
+    }
+    return array;
+}
+
+// метод, который будет печатать массив
+void PrintArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+// получение числа с консоли, для того, чтобы задать размерность массива
+int GetNumberFromConsole (string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+    while (!isCorrect)
+    {
+        Console.WriteLine (message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine ("Ввели не число. ВВедите число");
+        }
+    }
+    return result;
+}
+
+// метод, который будет переворачивать массив
+int [] ReverseArray (int [] array)
+{
+    int [] result = new int [array.Length];
+    int count = 0;
+    for (int i = array.Length - 1; i >= 0; i--)
+    {
+        result [count] = array [i];
+        count++;
+    }
+    return result;
+}
+
+int dimension = GetNumberFromConsole ("Введите размерность массива");
+int [] startArray = InitArray (dimension);
+PrintArray(startArray);
+int [] resultArray = ReverseArray (startArray);
+PrintArray(resultArray);
---
+++ Seminar_6/Zadacha_40/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задача №40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник со сторонами такой длины.
+*/
+
+int GetNumberFromConsole (string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+    while (!isCorrect)
+    {
+        Console.WriteLine (message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine ("Ввели не число. ВВедите число");
+        }
+    }
+    return result;
+}
+
+int a = GetNumberFromConsole ("Введите первую сторону треугольника: ");
+int b = GetNumberFromConsole ("Введите вторую сторону треугольника: ");
+int c = GetNumberFromConsole ("Введите третью сторону треугольника: ");
+
+if ((a + b) > c && (b + c) > a && (a + c) > b)
+{
+    Console.WriteLine("Треугольник существует: ");
+}
+else
+{
+    Console.WriteLine("Треугольник не существует: ");
+}
